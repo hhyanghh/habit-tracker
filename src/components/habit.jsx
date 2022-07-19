50de445adf06a96ab88d4d6ab50b5e0a3938fbe7
yanghyunhwa <yanghh711@naver.com>
2022-07-19 20:17:16 +0900

    habit component 만들기(state, function)

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 
 class Habit extends Component {
+    state = {
+        count: 0,
+    };
+
+    handelIncrement = () => {
+        // state  오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 한다.
+        this.setState({count: this.state.count + 1});
+    };
+
+    handelDecrement = () => {
+        const count = this.state.count -1;
+        this.setState({count: count < 0 ? 0 : count });
+    };
+
     render() {
         return (
             <li className="habit">
                 <span className="habit-name">Reading</span>
-                <span className="habit-count">8</span>
-                <button className="habit-button habit-increase">
+                <span className="habit-count">{this.state.count}</span>
+                <button className="habit-button habit-increase" onClick={this.handelIncrement}>
                     <i className="fa-solid fa-square-plus"></i>
                 </button>
-                <button className="habit-button habit-decrease">
+                <button className="habit-button habit-decrease" onClick={this.handelDecrement}>
                     <i className="fa-solid fa-square-minus"></i>
                 </button>
                 <button className="habit-button habit-delete">
