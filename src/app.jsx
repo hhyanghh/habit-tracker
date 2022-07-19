a464e0faef4b8390ac919c8a88522fa33514bf97
yanghyunhwa <yanghh711@naver.com>
2022-07-19 20:02:29 +0900

    habit tracker list 만들기

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,9 @@
-import './app.css';
+import Habit from './components/habit';
+import './app.css'
+
 
 function App() {
-  const name = 'elie';
-
-  return (
-      <>
-        <h1>hello world :)</h1>
-        {name && <h1>hello {name}</h1> }
-        {['⭐️','🧡'].map(item => (
-            <h1>{item}</h1>
-        ))}
-      </>
-  );
+  return <Habit></Habit>
 }
 
 export default App;
